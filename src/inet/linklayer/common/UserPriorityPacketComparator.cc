0b26010b47535a1c9354c55521883c8634879490
Levente Mészáros <levy@omnetpp.org>
2019-02-20 11:00:55 +0100

    Link: Added UserPriorityPacketComparator needed to restore original CsmaCaMac functionality.

---
+++ src/inet/linklayer/common/UserPriorityPacketComparator.cc
@@ -0,0 +1,49 @@
+//
+// Copyright (C) OpenSim Ltd.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see http://www.gnu.org/licenses/.
+//
+
+#include "inet/linklayer/common/UserPriority.h"
+#include "inet/linklayer/common/UserPriorityPacketComparator.h"
+#include "inet/linklayer/common/UserPriorityTag_m.h"
+
+namespace inet {
+
+Register_Class(UserPriorityPacketComparator);
+
+static int getPacketPriority(cObject *object)
+{
+    auto packet = static_cast<Packet *>(object);
+    int up = packet->getTag<UserPriorityReq>()->getUserPriority();
+    return (up == UP_BK) ? -2 : (up == UP_BK2) ? -1 : up;  // because UP_BE==0, but background traffic should have lower priority than best effort
+}
+
+static int comparePacketsByUserPriority(Packet *a, Packet *b)
+{
+    return getPacketPriority(b) - getPacketPriority(a);
+}
+
+int UserPriorityPacketComparator::comparePackets(Packet *a, Packet *b) const
+{
+    return comparePacketsByUserPriority(a, b);
+}
+
+inet::queue::PacketComparatorFunction UserPriorityPacketComparator::getComparatorFunction() const
+{
+    return &comparePacketsByUserPriority;
+}
+
+} // namespace inet
+
